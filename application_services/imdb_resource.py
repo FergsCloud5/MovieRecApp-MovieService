1a4cd08686bac293ef19d2ef91b2c4154271501f
Isabella Cho <isc2120@columbia.edu>
2021-10-12 08:27:30 -0400

    by MOVIE_ID

--- application_services/imdb_resource.py
+++ application_services/imdb_resource.py
@@ -8,11 +8,12 @@ class IMDBResource(BaseApplicationResource):
         super().__init__()
 
     @classmethod
-    def get_by_movie_id(cls, movie_id):
-        res = DbService.get_by_movie_id("imdb", "imdb_ajax_metadata", "movie_imdb_link", movie_id)
-        return res
-
-    @classmethod
     def get_by_template(cls, template):
         res = DbService.find_by_template("imdb", "imdb_ajax_metadata", template, None)
         return res
+
+    @classmethod
+    def get_by_movie_id(cls, movie_id):
+        res = DbService.get_by_movie_id("imdb", "imdb_ajax_metadata", "movie_id", movie_id)
+        return res
+
