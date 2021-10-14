8dc9c77b0994e849f9d192e69de57933aa65082b
Isabella Cho <isc2120@columbia.edu>
2021-10-14 12:04:24 -0400

    env var

--- middleware/context.py
+++ middleware/context.py
@@ -3,19 +3,16 @@ import os
 # This is a bad place for this import
 import pymysql
 
+
 def get_db_info():
     """
     This is crappy code.
     :return: A dictionary with connect info for MySQL
     """
 
-    # db_host = os.environ.get("DBHOST", None)
-    # db_user = os.environ.get("DBUSER", None)
-    # db_password = os.environ.get("DBPASSWORD", None)
-
-    db_host: "cloudcomp.cvrnijvhtjmj.us-east-2.rds.amazonaws.com"
-    db_user: "admin"
-    db_password: "IZZIcho1228"
+    db_host = os.environ.get("DBHOST", None)
+    db_user = os.environ.get("DBUSER", None)
+    db_password = os.environ.get("DBPASSWORD", None)
 
     if db_host is not None:
         db_info = {
